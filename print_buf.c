889e349c85698586103e927a94da8df448e2c2cb
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 00:49:57 +0000

    Update print_buf.c

--- print_buf.c
+++ print_buf.c
@@ -3,9 +3,10 @@
 /**
  * print_buf - prints buffer
  * @buf: buffer pointer
- * @nbuf: number of bytes to print
- * Return: number of bytes printed.
+ * @nbuf: number of the  bytes to print
+ * Return: number of the bytes printed.
  */
+
 int print_buf(char *buf, unsigned int nbuf)
 {
 	return (write(1, buf, nbuf));
