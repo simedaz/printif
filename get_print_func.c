e8f08f8a66a2af3fd1abd85f115a12a157ca597f
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 00:15:02 +0000

    Update get_print_func.c

--- get_print_func.c
+++ get_print_func.c
@@ -3,6 +3,7 @@
  * get_print_func - selects the correct function to perform the operation.
  * @s: argument indentifier
  * @index: index for argument indentifier
+ *
  * Return: pointer to a function.
  */
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
