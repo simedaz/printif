b522d2aef62079bba741e6aa9614e5a90b4b52cc
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 13:26:52 +0000

    Update print_short_int.c

--- print_short_int.c
+++ print_short_int.c
@@ -4,9 +4,10 @@
  * prinhint - prints a short integer
  * @arguments: input string
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * @ibuf: index for buffer the pointer
  * Return: number of chars printed.
  */
+
 int prinhint(va_list arguments, char *buf, unsigned int ibuf)
 {
 	short int int_input;
