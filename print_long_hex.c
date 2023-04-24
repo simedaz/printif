8e948202d6ade30d20b7afa008d6d437a534b927
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 12:50:37 +0000

    Update print_long_hex.c

--- print_long_hex.c
+++ print_long_hex.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * prinlhex - prints a long decimal in hexadecimal
  * @arguments: input string
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * @ibuf: index for the buffer pointer
+ * Return: number of the chars printed
  */
 int prinlhex(va_list arguments, char *buf, unsigned int ibuf)
 {
