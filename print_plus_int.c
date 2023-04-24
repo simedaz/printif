47b24696b23f0d74f2d734750f62b1d9e4ee126e
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 13:24:21 +0000

    Update print_plus_int.c

--- print_plus_int.c
+++ print_plus_int.c
@@ -3,10 +3,11 @@
 /**
  * prinpint - print integer with plus symbol
  * @arguments: input string
- * @buf: buffer pointer
+ * @buf: buffer of the  pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
+
 int prinpint(va_list arguments, char *buf, unsigned int ibuf)
 {
 	int int_input;
