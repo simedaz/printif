149d076a404c0771c1b7e04a66ecadc99fef3c30
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 00:47:43 +0000

    Update print_add.c

--- print_add.c
+++ print_add.c
@@ -4,10 +4,11 @@
  * print_add - prints the address of an input variable
  * @arguments: input address.
  * @buf: buffer pointer.
- * @ibuf: index for buffer pointer
+ * @ibuf: index for the buffer pointer
  *
  * Return: number of chars printed.
  */
+
 int print_add(va_list arguments, char *buf, unsigned int ibuf)
 {
 	void *add;
