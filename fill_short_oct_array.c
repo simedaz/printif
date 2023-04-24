b1046d6b941c17293efa4174c39736f977531ffc
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 00:14:04 +0000

    Update fill_short_oct_array.c

--- fill_short_oct_array.c
+++ fill_short_oct_array.c
@@ -16,9 +16,13 @@ char *fill_short_oct_array(char *bnr, char *oct)
 	for (i = 15, ioct = 5; i >= 0; i--, ioct--)
 	{
 		if (i > 0)
+		{
 			limit = 4;
+		}
 		else
+		{
 			limit = 1;
+		}
 		for (op = 0, j = 1; j <= limit; j *= 2, i--)
 			op = ((bnr[i] - '0') * j) + op;
 		i++;
