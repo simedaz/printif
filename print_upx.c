6935aeb526fadaec844dee1395c8053e74f49051
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 13:29:58 +0000

    Update print_upx.c

--- print_upx.c
+++ print_upx.c
@@ -5,7 +5,7 @@
  * @arguments: The character to print
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * Return: number of chars printed.
  */
 int print_upx(va_list arguments, char *buf, unsigned int ibuf)
 {
