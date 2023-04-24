ccf354307dc23e03260dd84c3367a00470a26437
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 00:15:52 +0000

    Update handl_buf.c

--- handl_buf.c
+++ handl_buf.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @c: charcter to concatenate
  * @ibuf: index of buffer pointer
+ *
  * Return: index of buffer pointer.
  */
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf)
