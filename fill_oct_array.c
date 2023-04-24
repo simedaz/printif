7e3418ecbeb18edee21b3f5cb81cb7739b1ab15a
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-04-24 00:12:52 +0000

    Update fill_oct_array.c

--- fill_oct_array.c
+++ fill_oct_array.c
@@ -16,11 +16,17 @@ char *fill_oct_array(char *bnr, char *oct)
 	for (i = 31, ioct = 10; i >= 0; i--, ioct--)
 	{
 		if (i > 1)
+		{
 			limit = 4;
+		}
 		else
+		{
 			limit = 2;
+		}
 		for (op = 0, j = 1; j <= limit; j *= 2, i--)
+		{
 			op = ((bnr[i] - '0') * j) + op;
+		}
 		i++;
 		oct[ioct] = op + '0';
 	}
